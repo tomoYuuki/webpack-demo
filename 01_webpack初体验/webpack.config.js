57bf55a39b09ea11b48304c2cd02fa0c4428f450
tomoYuuki <446679223@qq.com>
2023-03-17 15:06:38 +0800

    loader的use顺序

--- 01_webpack初体验/webpack.config.js
+++ 01_webpack初体验/webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
       {
         test: /\.css$/i,
         // css-loader只是解析.css文件，但是还需要style-loader帮你在元素中插入样式
+        // 注意：这里其实是有顺序的，都是从右往左，或者从下往上的，你得先解析了css，再去插入style
         use: ["style-loader", "css-loader"],
       },
     ],
