b81f0e630a1b40c893a2210b7f71201aef6f29ec
tomoYuuki <446679223@qq.com>
2023-03-19 19:15:30 +0800

    07_css-loader的配置项importLoaders了解

--- 01_webpack初体验/webpack.config.js
+++ 01_webpack初体验/webpack.config.js
@@ -14,22 +14,34 @@ module.exports = {
         // 注意：这里其实是有顺序的，都是从右往左，或者从下往上的，你得先解析了css，再去插入style
         use: [
           "style-loader",
-          "css-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              // 0 => no loaders (default);
+              // 1 => postcss-loader;
+            },
+          },
           "postcss-loader",
-          // {
-          //   loader: "postcss-loader",
-          //   options: {
-          //     postcssOptions: {
-          //       // plugins: [require("autoprefixer")],
-          //       plugins: ["postcss-preset-env"],
-          //     },
-          //   },
-          // },
         ],
       },
       {
         test: /\.less$/i,
-        use: ["style-loader", "css-loader", "less-loader", "postcss-loader"],
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+              // 0 => no loaders (default);
+              // 1 => postcss-loader;
+              // 2 => postcss-loader, sass-loader
+            },
+          },
+
+          "less-loader",
+          "postcss-loader",
+        ],
       },
     ],
   },
