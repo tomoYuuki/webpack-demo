0fce956dc0872950b2704e88f3c4a68115c9835e
tomoYuuki <446679223@qq.com>
2023-03-19 17:29:41 +0800

    05_认识postcss-loader-以及相关插件autoprefixer和postcss-preset-env

--- 01_webpack初体验/webpack.config.js
+++ 01_webpack初体验/webpack.config.js
@@ -12,7 +12,19 @@ module.exports = {
         test: /\.css$/i,
         // css-loader只是解析.css文件，但是还需要style-loader帮你在元素中插入样式
         // 注意：这里其实是有顺序的，都是从右往左，或者从下往上的，你得先解析了css，再去插入style
-        use: ["style-loader", "css-loader"],
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                // plugins: [require("autoprefixer")],
+                plugins: ["postcss-preset-env"],
+              },
+            },
+          },
+        ],
       },
       {
         test: /\.less$/i,
