a2a9f632534d189564cb451c4af674e42520ebc5
tomoYuuki <446679223@qq.com>
2023-03-20 19:56:40 +0800

    18.babel-loader,es6转es5

--- 02_webpack稍高级用法/src/common_index.js
+++ 02_webpack稍高级用法/src/common_index.js
@@ -2,3 +2,9 @@
 import {sub, add} from "./js/math.js";
 sub(1, 2);
 add1(3, 4);
+
+const func = () => {
+  console.log("say");
+};
+
+func();
--- 02_webpack稍高级用法/webpack.config.js
+++ 02_webpack稍高级用法/webpack.config.js
@@ -16,57 +16,67 @@ module.exports = {
   },
   module: {
     rules: [
+      // {
+      //   test: /\.css$/i,
+      //   // css-loader只是解析.css文件，但是还需要style-loader帮你在元素中插入样式
+      //   // 注意：这里其实是有顺序的，都是从右往左，或者从下往上的，你得先解析了css，再去插入style
+      //   use: [
+      //     "style-loader",
+      //     {
+      //       loader: "css-loader",
+      //       options: {
+      //         importLoaders: 1,
+      //         // 0 => no loaders (default);
+      //         // 1 => postcss-loader;
+      //       },
+      //     },
+      //     "postcss-loader",
+      //   ],
+      // },
+      // {
+      //   test: /\.less$/i,
+      //   use: [
+      //     "style-loader",
+      //     {
+      //       loader: "css-loader",
+      //       options: {
+      //         importLoaders: 2,
+      //         // 0 => no loaders (default);
+      //         // 1 => postcss-loader;
+      //         // 2 => postcss-loader, sass-loader
+      //       },
+      //     },
+      //     "less-loader",
+      //     "postcss-loader",
+      //   ],
+      // },
+      // {
+      //   test: /\.(jp?g|svg|gif|png)$/,
+      //   type: "asset",
+      //   parser: {
+      //     dataUrlCondition: {
+      //       maxSize: 100 * 1024, // 200kb 取代了file-loader的limit
+      //     },
+      //   },
+      //   generator: {
+      //     filename: "img/[name].[hash:6][ext]",
+      //   },
+      // },
+      // {
+      //   test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      //   type: "asset/resource",
+      //   generator: {
+      //     filename: "font/[name].[hash:6][ext]",
+      //   },
+      // },
       {
-        test: /\.css$/i,
-        // css-loader只是解析.css文件，但是还需要style-loader帮你在元素中插入样式
-        // 注意：这里其实是有顺序的，都是从右往左，或者从下往上的，你得先解析了css，再去插入style
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
-              // 0 => no loaders (default);
-              // 1 => postcss-loader;
-            },
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"], // 试了一下50版本都支持es6了 40多才不支持
           },
-          "postcss-loader",
-        ],
-      },
-      {
-        test: /\.less$/i,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 2,
-              // 0 => no loaders (default);
-              // 1 => postcss-loader;
-              // 2 => postcss-loader, sass-loader
-            },
-          },
-          "less-loader",
-          "postcss-loader",
-        ],
-      },
-      {
-        test: /\.(jp?g|svg|gif|png)$/,
-        type: "asset",
-        parser: {
-          dataUrlCondition: {
-            maxSize: 100 * 1024, // 200kb 取代了file-loader的limit
-          },
-        },
-        generator: {
-          filename: "img/[name].[hash:6][ext]",
-        },
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "font/[name].[hash:6][ext]",
         },
       },
     ],
