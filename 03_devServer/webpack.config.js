53f42c375cd5fd94c6fd1935c8338d9cf02c964a
tomoYuuki <446679223@qq.com>
2023-03-23 13:47:19 +0800

    31_devServer设置host，可以让本地的服务让外部访问到（前提是同一网络下）

--- 03_devServer/webpack.config.js
+++ 03_devServer/webpack.config.js
@@ -8,15 +8,16 @@ module.exports = {
   devServer: {
     port: 9000, // 设置端口
     hot: true, // 热更新开启
+    host: "0.0.0.0", // 如果你想让你的服务器可以被外部访问，像这样指定，默认就是localhost
     devMiddleware: {
-      publicPath: "/yk-path/", // 配置本地服务的基础路径 localhost:8080 ${publicPath} ...
+      publicPath: "/yk-path", // 配置本地服务的基础路径 localhost:8080 ${publicPath} ...
     },
   },
 
   output: {
     path: path.resolve(__dirname, "./build"),
     filename: "bundle.js",
-    publicPath: "/yk-path/", // html中 js/bundle.js  => ./js/bundle.js ,script引入打包后的包前面会加的路径，最好和devServer的一样
+    publicPath: "/yk-path", // html中 js/bundle.js  => ./js/bundle.js ,script引入打包后的包前面会加的路径，最好和devServer的一样
   },
   module: {
     rules: [
